277e888689ec844d5172baad5dedd201df0da29b
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-26 17:03:17 +0200

    updated C2PP comments

--- src/fMain.pas
+++ src/fMain.pas
@@ -1,42 +1,42 @@
-/// <summary>
-/// ***************************************************************************
-///
-/// Shift Keys Generator
-///
-/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// Use to generate a unique key composed of values between -7 and 7 for
-/// simple encryption and decryption of a set of bytes using the SHL and SHR
-/// binary operators.
-///
-/// If you're developing under Delphi or Pascal in general, you can use these blocks with the TOlfCryptDecrypt.ShiftCrypt and TOlfCryptDecrypt.ShiftDecrypt functions in the Olf.RTL.CryptDecrypt unit from https://github.com/DeveloppeurPascal/librairies/
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-/// Patrick PREMARTIN
-///
-/// Site :
-/// https://shiftkeysgenerator.olfsoftware.fr/
-///
-/// Project site :
-/// https://github.com/DeveloppeurPascal/Shift-Keys-Generator
-///
-/// ***************************************************************************
-/// File last update : 2024-08-07T08:10:28.000+02:00
-/// Signature : 0508dfd5bc190cbf61e4346619f4c43266179649
-/// ***************************************************************************
-/// </summary>
+(* C2PP
+  ***************************************************************************
+
+  Shift Keys Generator
+
+  Copyright 2024-2025 Patrick Prémartin under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Use to generate a unique key composed of values between -7 and 7 for
+  simple encryption and decryption of a set of bytes using the SHL and SHR
+  binary operators.
+
+  If you're developing under Delphi or Pascal in general, you can use these blocks with the TOlfCryptDecrypt.ShiftCrypt and TOlfCryptDecrypt.ShiftDecrypt functions in the Olf.RTL.CryptDecrypt unit from https://github.com/DeveloppeurPascal/librairies/
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://shiftkeysgenerator.olfsoftware.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Shift-Keys-Generator
+
+  ***************************************************************************
+  File last update : 2025-05-23T11:28:03.619+02:00
+  Signature : 084ab939513debd370ee8db6644668f99e97a370
+  ***************************************************************************
+*)
 
 unit fMain;
 
